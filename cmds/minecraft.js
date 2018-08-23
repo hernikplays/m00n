9ec7e58bb150e77aa51a8c51e36860a3703bb4ff
hernikplays <hernik27@gmail.com>
2018-08-23 19:39:31 +0100

    fixing

--- cmds/minecraft.js
+++ cmds/minecraft.js
@@ -12,7 +12,7 @@ module.exports.run = async (bot, message, args) => {
   if(args.join(" ").toLowerCase().includes("cake")) rnd = 10;
   if(args.join(" ").toLowerCase().includes("diamond")) rnd = 29;
 
-  if(title.length > 10 || contents.length > 10) return message.edit("Max Length: 10 Characters.");
+  if(title.length > 10 || contents.length > 10) return message.channel.send("Max Length: 10 Characters.");
   const url = `https://www.minecraftskinstealer.com/achievement/a.php?i=${rnd}&h=${encodeURIComponent(title)}&t=${encodeURIComponent(contents)}`;
   snekfetch.get(url)
    .then(r=>message.channel.send("", {files:[{attachment: r.body}]}));
