98d0d5fb87c7152567e10551cc91ce90a76287ad
hernikplays <hernik27@gmail.com>
2018-08-23 17:32:44 +0100

    added invite link

---
+++ cmds/invite.js
@@ -0,0 +1,9 @@
+let Discord = module.require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+    message.channel.send("You can invite me using this link: https://s.botempire.tk/yourls/potato")
+}
+
+module.exports.help = {
+    name: "invite"
+}
