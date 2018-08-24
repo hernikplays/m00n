03a59bb7ed688fac4fa9ac27a43f328750471267
hernikplays <hernik27@gmail.com>
2018-08-24 14:16:52 +0200

    fixing

--- cmds/play.js
+++ cmds/play.js
@@ -1,20 +1,22 @@
 const Discord = require("discord.js");
 const YTDL = require("ytdl-core");
 var servers = {};
-function play(){
-    var server = server[message.guild.id];
-    server.dispatcher = connection.playStream(YTDL(server.queue[0], {filter: "audioonly"}));
 
-    server.queue.shift();
-
-    server.dispatcher.on("end", function() {
-        if(server.queue[0]) play(connection, message);
-        else connection.disconnect; 
-    });
-}
 
 
 module.exports.run = async (bot, message, args) => {
+    function play(){
+        var server = server[message.guild.id];
+        server.dispatcher = connection.playStream(YTDL(server.queue[0], {filter: "audioonly"}));
+    
+        server.queue.shift();
+    
+        server.dispatcher.on("end", function() {
+            if(server.queue[0]) play(connection, message);
+            else connection.disconnect; 
+        });
+    }
+    
     if(!args[0]) return message.reply("You need to send a YouTube link!");
     if(!message.member.voiceChannel) return message.reply("Music is better played from a voice channel! Please join one first!");
     if(!servers[message.guild.id]) servers[message.guild.id] = {
@@ -24,7 +26,7 @@ module.exports.run = async (bot, message, args) => {
     var server = servers[message.guild.id];
 
     server.queue.push(args[0]);
-    
+
     if(!message.guild.voiceConnection) message.member.voiceChannel.join().then(function(connection){
         play(connection, message);
     });
