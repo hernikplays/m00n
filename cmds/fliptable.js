9ff5fb4a43c4ab5c6316d3f55fbd558a0dc4f920
hernikplays <hernik27@gmail.com>
2018-08-26 20:37:11 +0200

    Added fliptable command

---
+++ cmds/fliptable.js
@@ -0,0 +1,15 @@
+const Discord = module.require("discord.js");
+const fliptable = require("flip-table");
+module.exports.run = async (bot, message, args) => {
+    let returnmsg = fliptable("No text to flip!");
+    if(!args[0]) return message.reply(returnmsg);
+
+   let meme = fliptable(args.join(' '));
+   
+
+   message.channel.send(meme);
+}
+
+module.exports.help = {
+    name: "fliptable"
+}
