17a943775f53ee56758cd81b0563a03df3161b45
hernikplays <hernik27@gmail.com>
2018-08-26 21:39:43 +0200

    loggin

--- cmds/kym.js
+++ cmds/kym.js
@@ -2,10 +2,10 @@ const Discord = module.require("discord.js");
 const kym = require("nodeyourmeme");
 module.exports.run = async (bot, message, args) => {
     if(!args){
-        kym.random().then(message.channel.send(new Discord.RichEmbed().setTitle(name).setDescription(about))).catch(console.error);
+        kym.random().then(console.log).catch(console.error);
     }
     else if(args){
-        kym.search(args.join(' ')).then(message.channel.send(new Discord.RichEmbed().setTitle(name).setDescription(about))).catch(console.error);
+        kym.search(args.join(' ')).then(console.log).catch(console.error);
     }
 }
 
