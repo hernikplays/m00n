1862764d599e4664c735617e051e723067a4d2bc
hernikplays <hernik27@gmail.com>
2018-08-26 21:35:48 +0200

    testing KnowYourMeme command

---
+++ cmds/kym.js
@@ -0,0 +1,14 @@
+const Discord = module.require("discord.js");
+const kym = require("nodeyourmeme");
+module.exports.run = async (bot, message, args) => {
+    if(!args){
+        kym.random().then(message.channel.send(new Discord.RichEmbed().setTitle(name).setDescription(about))).catch(console.error);
+    }
+    else if(args){
+        kym.search(args.join(' ')).then(message.channel.send(new Discord.RichEmbed().setTitle(name).setDescription(about))).catch(console.error);
+    }
+}
+
+module.exports.help = {
+    name: "kym"
+}
