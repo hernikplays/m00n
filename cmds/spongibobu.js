150c457a0417a2d0d7a58db04f3023073a0e97e3
hernikplays <hernik27@gmail.com>
2018-08-26 20:15:24 +0200

    fix

--- cmds/spongibobu.js
+++ cmds/spongibobu.js
@@ -1,13 +1,13 @@
 const Discord = module.require("discord.js");
 var spongi = require("spongibobu.js");
 module.exports.run = async (bot, message, args) => {
-  let returnmsg = spongi("You need a message to 'spongi'!"); // Don't know if too lazy or don't know how the meme exactly goes!
+  let returnmsg = spongi("You need a message to 'spongibobify '!"); // Don't know if too lazy or don't know how the meme exactly goes!
 
   if(!args[0]) return message.reply(returnmsg);
 
-   let meme = spongi(args[0]);
+   let meme = spongi(args);
 
-   
+
    message.channel.send(meme);
    
 }
