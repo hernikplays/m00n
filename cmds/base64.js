e159836da0dbc418db69c5de78d1cadc6541c058
hernikplays <hernik27@gmail.com>
2018-08-27 22:25:43 +0200

    replaced

--- cmds/base64.js
+++ cmds/base64.js
@@ -1,11 +1,11 @@
 const Discord = module.require("discord.js");
-const text = require("text");
+const bsf = require("libbase64");
 const Webhook = require("webhook-discord")
  
 const Hook = new Webhook(process.env.WBHK)
 module.exports.run = async (bot, message, args) => {
    if(!args[0]) return message.reply("You need to supply text to encode!")
-   let encode = text.urlsafeEncode64(args.join(' '));
+   let encode = bsf.encode(args.join(' '));
    if(err) return Hook.info(`There was an error in the base64 command: ${err}`,"Error");
    message.channel.send(encode)
 }
--- cmds/unbase64.js
+++ cmds/unbase64.js
@@ -1,11 +1,11 @@
 const Discord = module.require("discord.js");
-const text = require("text");
+const bsf = require("libbase64");
 const Webhook = require("webhook-discord")
  
 const Hook = new Webhook(process.env.WBHK)
 module.exports.run = async (bot, message, args) => {
    if(!args[0]) return message.reply("You need to supply encoded text to decode!")
-   let decode = text.urlsafeDecode64(args.join(' '));
+   let decode = bsf.decode(args.join(' '));
    if(err) return Hook.info(`There was an error in the base64 command: ${err}`,"Error");
    message.channel.send(decode)
 }
