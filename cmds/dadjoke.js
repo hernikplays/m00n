f9b798b7fd68b022267d2ae56033af0ee4896354
hernikplays <hernik27@gmail.com>
2018-08-28 11:22:42 +0200

    fixx

--- cmds/dadjoke.js
+++ cmds/dadjoke.js
@@ -4,9 +4,9 @@ const superagent = require("snekfetch");
 module.exports.run = async (bot, message, args) => {
     superagent.get('https://icanhazdadjoke.com/')
         .end((err, response) => {
+            console.log(response);
       const e = new Discord.RichEmbed()
-      .setTitle("Here is your dad joke")
-      .addField(response.body.text)
+      .addField("Here is your dad joke", response.body.text)
       .setColor(`RANDOM`)
       .setFooter("Supplied by icanhazdadjoke.com")
   message.channel.send(e);
