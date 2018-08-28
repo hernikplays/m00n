5712d5ec509a1ccc08f499ee94cc976a333d57dd
hernikplays <hernik27@gmail.com>
2018-08-28 10:18:11 +0200

    rewrote

--- cmds/weather.js
+++ cmds/weather.js
@@ -1,5 +1,4 @@
 const Discord = module.require("discord.js");
-var weather = require('weather-js');
 const Webhook = require("webhook-discord")
  
 const Hook = new Webhook(process.env.WBHK)
@@ -7,24 +6,25 @@ module.exports.run = async (bot, message, args) => {
     if(!message.author.id == "145973959127597057") return message.reply("This command is being tested")
     if(!args[0]) return message.reply("You need to enter a location!");
     if(!args[1]){
-        weather.find({search: args.join(' '), degreeType: 'C'}, function(err, result) {
-            if(err) Hook.err(`There was an error in the weather command: ${err}`,"JustAPotato");
-            
+        
+        let wem = new Discord.RichEmbed()
+        .setTitle(`Weather forecast for ${args}`)
+        .setImage(`https://wttr.in/${args}.png`)
+        .setFooter("Powered by wttr.in", "https://s8.postimg.cc/7i7ntzih1/wttr_in.png")
+        message.channel.send(wem);
             return;
-    });
+  
     }
     else if(args[1] == "F"){
-    weather.find({search: args.join(' ').substring("1", "0"), degreeType: 'F'}, function(err, result) {
-        if(err) Hook.err(`There was an error in the weather command: ${err}`,"JustAPotato");
     
-        let stringif = JSON.stringify(result, null, 2);
         let wem = new Discord.RichEmbed()
-        .setTitle(`Current weather in ${stringif.location.name}`)
-        .addField("Current Temperature",`${stringif.current.temperature}°F`)
-        .addField("Sky", stringif.current.skytext)
-        .addField("Wind Speed & Location", stringif.current.winddisplay)
+        .setTitle(`Weather forecast for ${args}`)
+        .setImage(`https://wttr.in/${args}.png?u`)
+        .setFooter("Powered by wttr.in", "https://s8.postimg.cc/7i7ntzih1/wttr_in.png")
         message.channel.send(wem);
-    });}
+        return;
+   
+        }
 }
 
 module.exports.help = {
