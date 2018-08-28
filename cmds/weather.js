80c20fb77949affbf79881d48c1f1610ee3381ee
hernikplays <hernik27@gmail.com>
2018-08-28 09:47:46 +0200

    fix

--- cmds/weather.js
+++ cmds/weather.js
@@ -5,13 +5,18 @@ const Webhook = require("webhook-discord")
 const Hook = new Webhook(process.env.WBHK)
 module.exports.run = async (bot, message, args) => {
     if(!args[0]) return message.reply("You need to enter a location!");
-    if(!args[1]) let degree = "C";
-    if(args[2]) let location = args[0] + args[1];
-    weather.find({search: 'San Francisco, CA', degreeType: 'F'}, function(err, result) {
+    if(!args[1]){
+        weather.find({search: args.join(' '), degreeType: 'C'}, function(err, result) {
+            if(err) Hook.err(`There was an error in the weather command: ${err}`,"JustAPotato");
+            console.log(JSON.stringify(result, null, 2));
+    });
+    }
+    else{
+    weather.find({search: args.join(' ').substring("1", "0"), degreeType: 'F'}, function(err, result) {
         if(err) Hook.err(`There was an error in the weather command: ${err}`,"JustAPotato");
     
         console.log(JSON.stringify(result, null, 2));
-    });
+    });}
 }
 
 module.exports.help = {
