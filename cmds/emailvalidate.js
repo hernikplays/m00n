2c1e38afceb73b3d5693b895b6855303a7341242
hernikplays <hernik27@gmail.com>
2018-08-28 14:11:37 +0200

    test

--- cmds/emailvalidate.js
+++ cmds/emailvalidate.js
@@ -4,9 +4,9 @@ module.exports.run = async (bot, message, args) => {
     if(!args[0]) return message.reply('You need to specify an e-mail')
     unirest.get(`https://pozzad-email-validator.p.mashape.com/emailvalidator/validateEmail/${args.join(' ')}`)
     .header("X-Mashape-Key", process.env.APIKEY)
-    .header("Accept", "text/plain")
+    .header("Accept", "application/json")
     .end(function (result) {
-      console.log(result);
+      console.log(result.status, result.headers, result.body);
     });
 }
 
