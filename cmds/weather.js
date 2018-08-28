0c48264419cb8e08469cd6f72074190c513899f1
hernikplays <hernik27@gmail.com>
2018-08-28 10:26:16 +0200

    fixing

--- cmds/weather.js
+++ cmds/weather.js
@@ -3,20 +3,22 @@ const Webhook = require("webhook-discord")
  
 const Hook = new Webhook(process.env.WBHK)
 module.exports.run = async (bot, message, args) => {
+    
     if(!message.author.id == "145973959127597057") return message.reply("This command is being tested")
     if(!args[0]) return message.reply("You need to enter a location!");
     if(!args[1]){
-        
+        let nope = args.join(' ');
         let wem = new Discord.RichEmbed()
-        .setTitle(`Weather forecast for ${args}`)
-        .setImage(`https://wttr.in/${args}.png`)
+        .setTitle(`Weather forecast for ${nope}`)
+        .setImage(`https://wttr.in/${nope}.png`)
         .setFooter("Powered by wttr.in", "https://s8.postimg.cc/7i7ntzih1/wttr_in.png")
         message.channel.send(wem);
             return;
   
     }
     else if(args[1] == "F"){
-    let nope = args.substring(2,0);
+        let yeet = args.join(' ')
+    let nope = yeet.substring(2,0);
         let wem = new Discord.RichEmbed()
         .setTitle(`Weather forecast for ${nope}`)
         .setImage(`https://wttr.in/${nope}.png?u`)
