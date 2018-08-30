0f2e179c78977631148ffae285489006f4b37fd1
hernikplays <hernik27@gmail.com>
2018-08-30 14:35:18 +0200

    test

---
+++ cmds/dadjoke.js
@@ -0,0 +1,18 @@
+const Discord = module.require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+    unirest.get(`https://icanhazdadjoke.com/`)
+    .header("Accept", "text/plain")
+    .end(function (result) {
+        console.log(result.body)
+      let e = new Discord.RichEmbed()
+      .addField("Here is your dad joke", `**${result.body}**`)
+      .setFooter("Dad jokes dad-livered by icanhazdadjoke.com")
+      message.channel.send(e);
+      
+    });
+}
+
+module.exports.help = {
+    name: "dadjoke"
+}
