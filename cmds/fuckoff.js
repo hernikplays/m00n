ac6510669f93f854a1b7bffb49e75f60402fa5f1
hernikplays <hernik27@gmail.com>
2018-08-30 13:10:36 +0200

    added fuckoff fully

--- cmds/fuckoff.js
+++ cmds/fuckoff.js
@@ -1,12 +1,33 @@
 const Discord = module.require("discord.js");
 const unirest = require("unirest")
 module.exports.run = async (bot, message, args) => {
-    if(!args[0]) return message.reply('You need to specify an e-mail')
-    unirest.get(`https://www.foaas.com/fyyff/${message.author.username}`)
-    .header("X-Mashape-Key", process.env.APIKEY)
+    var fuckoff = [
+        'everyone',
+        'asshole',
+        'bag',
+        'bye',
+        'cup',
+        'diabetes',
+        'everything',
+        'fascinating',
+        'flying',
+        'fyyff',
+        'give',
+        'immensity',
+        'looking',
+        'maybe',
+        'thanks',
+        'too',
+        'zayn'
+    ]
+    var fo = fuckoff[Math.round(Math.random() * (fuckoff.length - 1))];
+    unirest.get(`https://www.foaas.com/${fo}/${message.author.username}`)
     .header("Accept", "text/plain")
     .end(function (result) {
-      console.log(result.body);
+      let e = new Discord.RichEmbed()
+      .addField(result.body)
+      .setFooter("Delivered with fuckin' love by foaas.com")
+      message,channel.send(e);
       
     });
 }
