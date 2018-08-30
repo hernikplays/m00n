3600dfae6db0f5b0eeb19b7efe9c8e76731da42e
hernikplays <hernik27@gmail.com>
2018-08-30 13:16:20 +0200

    qikfix

--- cmds/fuckoff.js
+++ cmds/fuckoff.js
@@ -25,7 +25,7 @@ module.exports.run = async (bot, message, args) => {
     .header("Accept", "text/plain")
     .end(function (result) {
       let e = new Discord.RichEmbed()
-      .addField(result.body)
+      .addField("Here is your fuck off", `**${result.body}**`)
       .setFooter("Delivered with fuckin' love by foaas.com")
       message.channel.send(e);
       
