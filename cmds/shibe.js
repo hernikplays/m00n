7fb6519b5dc029c4f8d9c7e388ced4aebf93e23a
Discordies <41467532+discordies@users.noreply.github.com>
2018-09-15 21:07:48 +0200

    trying with unirest

--- cmds/shibe.js
+++ cmds/shibe.js
@@ -1,20 +1,24 @@
 const talkedRecently = new Set();
 const Discord = module.require("discord.js");
 const superagent = require("superagent");
+let unirest = require("unirest");
 module.exports.run = async (bot, message, args) => {
     if (talkedRecently.has(message.author.id)) {
         return message.channel.send(`${message.author.username} please wait 15 seconds before using that command again!`);
     }
     else {
-    let {
-        body
-    } = await superagent
-        .get(`http://shibe.online/api/shibes`);
-    const catembed = new Discord.RichEmbed()
-        .setTitle('Wow!')
-        .setColor("RANDOM")
-        .setImage(body.file)
-    message.channel.send(catembed);
+        unirest.get(`http://shibe.online/api/shibe`)
+        .header("Accept", "text/plain")
+        .end(function (result) {
+            console.log(result.body)
+          let e = new Discord.RichEmbed()
+          .setDescription("Wow!")
+          .setImage(result.body)
+          .setFooter("Delivered with fuckin' love by foaas.com")
+          
+          message.channel.send(e);
+          
+        });
 
     talkedRecently.add(message.author.id); //cooldown
         setTimeout(() => {
