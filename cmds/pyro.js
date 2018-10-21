5dc6b086301ea9aa89da1aee6a6a6227a66c18db
hernikplays <hernik27@gmail.com>
2018-10-21 19:58:45 +0100

    added pyro pic

--- cmds/pyro.js
+++ cmds/pyro.js
@@ -10,14 +10,15 @@ module.exports.run = async (bot, message, args) => {
         "https://i.ytimg.com/vi/T7TWe3aiYqg/maxresdefault.jpg",
         "https://media.tenor.com/images/6cdd45fbe9f4eb800f69587d051d5562/tenor.gif",
         "https://cdn.discordapp.com/attachments/469680760232738816/483974855184744498/unknown.png",
-        "https://cdn.discordapp.com/attachments/469680760232738816/485869489553014794/Bed.PNG"
+        "https://cdn.discordapp.com/attachments/469680760232738816/485869489553014794/Bed.PNG",
+        "https://letsgetthis.bread.moe/gflw8.png"
       ]
       let result = Math.floor((Math.random() * pyromemes.length));
       const e = new Discord.RichEmbed()
       .setColor(`RANDOM`)
       .setTitle(`Here you go!`)
       .setImage(pyromemes[result])
-      .setFooter("Want to submit something to the bot? Go to our bug tracker, and request it!")
+      .setFooter("Sub to pyro!")
       message.channel.send({
       embed: e
   })
