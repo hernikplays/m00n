2668b7f1966d52bdb775bb19abc68939d25e9b20
Discordies <41467532+discordies@users.noreply.github.com>
2018-10-29 21:02:15 +0100

    ship might fixed now

--- cmds/ship.js
+++ cmds/ship.js
@@ -23,17 +23,17 @@ module.exports.run = async (bot, message, args) => {
         const mUserA = mUser.avatarURL
         var imagetobase = `https://www.botempire.tk/i/test.png`;
         Jimp.read(mUser.avatarURL, function (err, imagetouse) {
-            
+            if (err) throw err;
             imagetouse.quality(60)
                       .resize(866, 866)
                       .write("imagetouse.jpg");
             Jimp.read(mUser2.avatarURL, function (err, imagetouse2) {
-            
+            if (err) throw err;
             imagetouse2.quality(60)
                       .resize(866, 866)
                       .write("imagetouse2.jpg");
             Jimp.read(imagetobase, function (err, mydude) {
-              
+                if (err) throw err;
                 Jimp.loadFont(Jimp.FONT_SANS_64_WHITE).then(function (font) {
                     mydude.quality(60)
                     mydude.print(font, 635, 1717, `${message.author.username}`)
@@ -41,21 +41,20 @@ module.exports.run = async (bot, message, args) => {
                     mydude.composite( imagetouse2, 2191, 797)
                     mydude.write("saveme.jpg");
                     mydude.getBuffer(`image/jpeg`, (err, buf) => {
-                       
+                        if (err) return err;
                         message.channel.send({files: [{attachment: buf, name: `saveme.jpg`}] });
                         message.channel.send(replies[result]);
-
-                        talkedRecently.add(message.author.id); //cooldown
-        setTimeout(() => {
-         
-          talkedRecently.delete(message.author.id);
-        }, 15000);
                         
                     });
                 });
             });}
         )});
-        }
+        talkedRecently.add(message.author.id); //cooldown
+        setTimeout(() => {
+         
+          talkedRecently.delete(message.author.id);
+        }, 15000);
+    }
 }
 
 module.exports.help = {
