4789dae46fc0eae3ce46e2f89f06bd0a9b4a4ecb
hernikplays <hernik27@gmail.com>
2018-12-13 19:49:41 +0100

    fix links

--- cmds/vote.js
+++ cmds/vote.js
@@ -1,7 +1,7 @@
 const Discord = module.require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
-    message.channel.send("All voting links can be found on my website: https://jap.hernikplays.tk/doc/vote")
+    message.channel.send("All voting links can be found on my website: https://jap.hernikplays.cz/doc/vote")
 }
 
 module.exports.help = {
--- cmds/website.js
+++ cmds/website.js
@@ -1,7 +1,7 @@
 const Discord = module.require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
-    message.channel.send("Bot's website: https://jap.hernikplays.tk")
+    message.channel.send("Bot's website: https://jap.hernikplays.cz")
 }
 
 module.exports.help = {
