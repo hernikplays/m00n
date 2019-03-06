e85d8653f185cf44aef83b17ede3a9cdd29ad592
hernikplays <hernik27@gmail.com>
2019-03-06 18:31:56 +0100

    works

--- cmds/chucknorris.js
+++ cmds/chucknorris.js
@@ -4,7 +4,6 @@ module.exports.run = async (bot, message, args) => {
     unirest.get(`http://api.chucknorris.io/jokes/random`)
     .header("Accept", "application/json")
     .end(function (result) {
-        console.log(result)
       let e = new Discord.RichEmbed()
       .addField("Here is your Chuck Norris Fact: ", `**${result.body.value}**`)
       .setFooter("Facts from https://chucknorris.io","https://assets.chucknorris.host/img/avatar/chuck-norris.png")
