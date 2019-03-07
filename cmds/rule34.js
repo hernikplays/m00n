53900909dd199f336b5880a06a10cb4daa84ddb6
hernikplays <hernik27@gmail.com>
2019-03-07 16:32:53 +0100

    ratelimit

--- cmds/rule34.js
+++ cmds/rule34.js
@@ -5,6 +5,10 @@ const moreSites = require('../r34.json');
 const kaori = new Kaori(moreSites);
 
 module.exports.run = async (bot, message, args) => {
+    if (talkedRecently.has(message.author.id)) {
+        return message.channel.send(`${message.author.username} please wait 7 seconds before using that command again!`);
+    }
+    else {
     if(!message.channel.nsfw) return message.channel.send(":no_entry: You need to be in a NSFW channel")
     if(!args[0]) return message.channel.send("🔍 You need to enter the search term!");
     let ask = args.toString();
@@ -16,6 +20,12 @@ module.exports.run = async (bot, message, args) => {
         message.channel.send({file: images[0].common.fileURL})
         )
     .catch(err => message.channel.send(`There was an error while processing your request: ${err}`));
+    talkedRecently.add(message.author.id); //cooldown
+        setTimeout(() => {
+          
+          talkedRecently.delete(message.author.id);
+        }, 7000);
+    }
 }
 
 module.exports.help = {
