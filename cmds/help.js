1d3237a4697413d44e2e4e70dbbd429dc8e2e9c9
Hernikplays <hernik@hernikplays.cz>
2019-10-10 19:52:57 +0200

    help link

--- cmds/help.js
+++ cmds/help.js
@@ -1,12 +1,12 @@
 let Discord = module.require("discord.js");
-const theprefix = "pt!" // Here it is just so I don't have to rewrite the footer
+const theprefix = "m!" // Here it is just so I don't have to rewrite the footer
 module.exports.run = async (bot, message, args) => {
     
             
           let em2 = new Discord.RichEmbed()
           
           .setColor("#FFFF7F")
-          .addField("Help", "Full help avalible [here](https://jap.hernikplays.cz/)")
+          .addField("Help", "Full help avalible [here](https://hernikplays.cz/m00n)")
            
             .setFooter(`Bot Prefix: ${theprefix} -- <required> [optional] -- created by hernikplays#9617`)
             await message.channel.send(em2);
