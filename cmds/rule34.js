aa1e9e65f81306bdfbee2d1b7933c2b44f2a01bb
Hernikplays <hernik@hernikplays.cz>
2019-10-10 19:42:03 +0200

    added console log

--- cmds/rule34.js
+++ cmds/rule34.js
@@ -6,30 +6,34 @@ const kaori = new Kaori(moreSites);
 
 
 module.exports.run = async (bot, message, args) => {
-     
-    if(!message.channel.nsfw) return message.channel.send(":no_entry: You need to be in a NSFW channel")
-    if(!args[0]) return message.channel.send("🔍 You need to enter the search term!");
+
+    if (!message.channel.nsfw) return message.channel.send(":no_entry: You need to be in a NSFW channel")
+    if (!args[0]) return message.channel.send("🔍 You need to enter the search term!");
     let ask = args.toString();
-    let search = ask.replace(",","");
-    
+    let search = ask.replace(",", "");
+
     var r34sites = [
         'r34',
         'e621'
     ]
     var chooser34 = r34sites[Math.round(Math.random() * (r34sites.length - 1))];
+    console.log(chooser34);
+    kaori.search(chooser34, {
+            tags: [search],
+            limit: 1,
+            random: true
+        }).then(images => {
+            let wem = new Discord.RichEmbed()
+                .setTitle(`Here is your porn`)
+                .setImage(images[0].common.fileURL)
+                .setColor("RANDOM")
+                .setFooter(`Requested by ${message.author.username}`, "https://images.emojiterra.com/twitter/v12/512px/1f51e.png")
+            message.channel.send(wem);
+        })
+        .catch(err => message.channel.send(`There was an error while processing your request: ${err}`));
 
-    kaori.search(chooser34, { tags: [search], limit: 1, random: true }).then(images => {
-        let wem = new Discord.RichEmbed()
-        .setTitle(`Here is your porn`)
-        .setImage(images[0].common.fileURL)
-        .setColor("RANDOM")
-        .setFooter(`Requested by ${message.author.username}`, "https://images.emojiterra.com/twitter/v12/512px/1f51e.png")
-        message.channel.send(wem);
-    })
-    .catch(err => message.channel.send(`There was an error while processing your request: ${err}`));
 
-    
-    }
+}
 
 
 module.exports.help = {
