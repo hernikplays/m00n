72f9c805d5d21f0c061b368e72f2e60bb0f1e77a
Hernikplays <hernik@hernikplays.cz>
2019-10-11 14:03:19 +0200

    capital

--- cmds/apod.js
+++ cmds/apod.js
@@ -5,7 +5,7 @@ module.exports.run = async (bot, message, args) => {
     .header("Accept", "application/json")
     .end(function (result) {
       let e = new Discord.RichEmbed()
-      .setTitle(`Nasa's Astronomy Picture of the Day: '${result.body.title}'`)
+      .setTitle(`NASA's Astronomy Picture of the Day: '${result.body.title}'`)
       .setImage(result.body.url)
       .setColor("#0066B1")
       .setFooter(`Photo (C) ${result.body.copyright}`,"https://www.stickpng.com/assets/images/58429400a6515b1e0ad75acc.png")
