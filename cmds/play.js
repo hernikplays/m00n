142917bc5eacf2f3ce1d52670ab5f6b4d87fb01c
Hernik_plays <hernik@hernikplays.cz>
2019-10-12 20:44:17 +0200

    preparing a giveaway command (?)

--- cmds/play.js
+++ cmds/play.js
@@ -4,17 +4,17 @@ var servers = {};
 
 
 
-module.exports.run = async (bot, message, args) => {
-    if(!message.author.id == "145973959127597057") return message.reply("This command is currently limited due to limits of our (free) hosting. But you can donate to our patreon https://patreon.com/discordies and may get access!")
-    if(!args[0]) return message.reply("You need to send a YouTube link!");
-    if(!message.member.voiceChannel) return message.reply("Music is better played from a voice channel! Please join one first!");
+module.exports.run = async(bot, message, args) => {
+    if (!message.author.id == "145973959127597057") return message.reply("This command is currently in beta. But you can donate to my patreon https://patreon.com/hernikplays and get access!")
+    if (!args[0]) return message.reply("You need to send a YouTube link!");
+    if (!message.member.voiceChannel) return message.reply("Music is better played from a voice channel! Please join one first!");
     let validate = await YTDL.validateURL(args[0]);
-    if(!validate) return message.reply("Please enter a valid URL");
+    if (!validate) return message.reply("Please enter a valid URL");
     let info = await YTDL.getInfo(args[0]);
     let connection = await message.member.voiceChannel.join();
     message.channel.send(`Now playing: ${info.title}`);
-    let dispatcher = await connection.playStream(YTDL(args[0], {filter: "audioonly"}));
-    
+    let dispatcher = await connection.playStream(YTDL(args[0], { filter: "audioonly" }));
+
 }
 
 module.exports.help = {
--- cmds/stop.js
+++ cmds/stop.js
@@ -3,22 +3,23 @@ const YTDL = require("ytdl-core");
 var servers = {};
 
 
-module.exports.run = async (bot, message, args) => {
-    if(!message.author.id == "145973959127597057") return message.reply("This command is currently in testing limited due to limits of our (free) hosting")
+module.exports.run = async(bot, message, args) => {
+    if (!message.author.id == "145973959127597057") return message.reply("This command is currently in beta. But you can donate to my patreon https://patreon.com/hernikplays and get access!")
+
     function play(connection, message) {
         var server = server[message.guild.id];
-        server.dispatcher = connection.playStream(YTDL(server.queue[0], {filter: "audioonly"}));
-    
+        server.dispatcher = connection.playStream(YTDL(server.queue[0], { filter: "audioonly" }));
+
         server.queue.shift();
-    
+
         server.dispatcher.on("end", function() {
-            if(server.queue[0]) play(connection, message);
-            else connection.disconnect; 
+            if (server.queue[0]) play(connection, message);
+            else connection.disconnect;
         });
     }
     var server = servers[message.guild.id];
 
-    if(message.guild.voiceConnection) message.guild.voiceConnection.disconnect();
+    if (message.guild.voiceConnection) message.guild.voiceConnection.disconnect();
     message.channel.send("Left the voice channel");
 
 }
