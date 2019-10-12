f39c1f053b0bf26392d5a316c1e0e8cc51e2775a
Hernik_plays <hernik@hernikplays.cz>
2019-10-12 21:26:06 +0200

    added porn gif command cause why not

---
+++ cmds/porn.js
@@ -0,0 +1,20 @@
+const Discord = module.require("discord.js");
+const unirest = require("unirest")
+module.exports.run = async(bot, message, args) => {
+    if (!message.channel.nsfw) return message.channel.send(":no_entry: You need to be in a NSFW channel")
+    unirest.get(`https://api.ramfish.tk/porn`)
+        .header("Accept", "application/json")
+        .end(function(result) {
+            let e = new Discord.RichEmbed()
+                .setTitle(`Here's your Porn GIF`)
+                .setImage(result.body.url)
+                .setColor("#AE090D")
+                .setFooter(`Requested by ${message.author.username}`, "https://images.emojiterra.com/twitter/v12/512px/1f51e.png")
+            message.channel.send(e);
+
+        });
+}
+
+module.exports.help = {
+    name: "porn"
+}
