00620cabd0fbb8bc5edb562a51e26fd326c623cf
Hernik_plays <hernik@hernikplays.cz>
2019-10-12 21:54:28 +0200

    replaced porn with yiff
    please lord forgive me

--- cmds/yiff.js
+++ cmds/yiff.js
@@ -2,12 +2,12 @@ const Discord = module.require("discord.js");
 const unirest = require("unirest")
 module.exports.run = async(bot, message, args) => {
     if (!message.channel.nsfw) return message.channel.send(":no_entry: You need to be in a NSFW channel")
-    unirest.get(`https://api.ramfish.tk/porn`)
+    unirest.get(`https://sheri.bot/api/yiff/`)
         .header("Accept", "application/json")
         .end(function(result) {
             console.log(result.body)
             let e = new Discord.RichEmbed()
-                .setTitle(`Here's your Porn GIF`)
+                .setTitle(`Here's your Yiff`)
                 .setImage(result.body.url)
                 .setColor("#AE090D")
                 .setFooter(`Requested by ${message.author.username}`, "https://images.emojiterra.com/twitter/v12/512px/1f51e.png")
@@ -17,5 +17,5 @@ module.exports.run = async(bot, message, args) => {
 }
 
 module.exports.help = {
-    name: "porn"
+    name: "yiff"
 }
