09976bda27a155f74fd4df0da76af321120ee30d
Hernik_plays <hernik@hernikplays.cz>
2019-10-12 20:50:35 +0200

    doing it later, request stuff in issues

--- cmds/giveaway.js
+++
@@ -1,9 +0,0 @@
-const Discord = module.require("discord.js");
-
-module.exports.run = async(bot, message, args) => {
-    if (!message.author.id == "145973959127597057") return message.reply("This command is currently in beta. But you can donate to my patreon https://patreon.com/hernikplays and get early access!")
-}
-
-module.exports.help = {
-    name: "giveaway"
-}
