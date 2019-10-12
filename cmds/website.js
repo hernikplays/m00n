f6a8c25eda4ea40c0587682431c6c8e5e88196cb
Hernik_plays <hernik@hernikplays.cz>
2019-10-12 22:03:51 +0200

    repo

--- cmds/website.js
+++ cmds/website.js
@@ -1,7 +1,7 @@
 const Discord = module.require("discord.js");
 
-module.exports.run = async (bot, message, args) => {
-    message.channel.send("Bot's website: https://hernikplays.cz/m00n")
+module.exports.run = async(bot, message, args) => {
+    message.channel.send("Bot's website: https://hernikplays.cz/m00n and GitHub repo: https://github.com/hernikplays/m00n")
 }
 
 module.exports.help = {
