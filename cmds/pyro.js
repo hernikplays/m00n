22d74fd13b96684cf7580d4aa97c09f8b1da4925
Hernik_plays <hernik@hernikplays.cz>
2019-10-12 22:25:14 +0200

    stole more pyro images from twitter

--- cmds/pyro.js
+++ cmds/pyro.js
@@ -1,6 +1,6 @@
 let Discord = module.require("discord.js");
 
-module.exports.run = async (bot, message, args) => {
+module.exports.run = async(bot, message, args) => {
     let pyromemes = [
         "https://i2.kym-cdn.com/photos/images/original/001/220/720/569.gif",
         "https://i.ytimg.com/vi/mLY4p_W6RB0/hqdefault.jpg",
@@ -11,16 +11,18 @@ module.exports.run = async (bot, message, args) => {
         "https://media.tenor.com/images/6cdd45fbe9f4eb800f69587d051d5562/tenor.gif",
         "https://cdn.discordapp.com/attachments/469680760232738816/483974855184744498/unknown.png",
         "https://cdn.discordapp.com/attachments/469680760232738816/485869489553014794/Bed.PNG",
-      ]
-      let result = Math.floor((Math.random() * pyromemes.length));
-      const e = new Discord.RichEmbed()
-      .setColor(`RANDOM`)
-      .setTitle(`Here you go!`)
-      .setImage(pyromemes[result])
-      .setFooter("Sub to pyro!")
-      message.channel.send({
-      embed: e
-  })
+        "https://i.postimg.cc/nc86v2d8/pyrostair.jpg",
+        "https://i.postimg.cc/0QMBSKD2/pyroalex.jpg"
+    ]
+    let result = Math.floor((Math.random() * pyromemes.length));
+    const e = new Discord.RichEmbed()
+        .setColor(`RANDOM`)
+        .setTitle(`Here you go!`)
+        .setImage(pyromemes[result])
+        .setFooter("Sub to pyro!")
+    message.channel.send({
+        embed: e
+    })
 
 }
 
