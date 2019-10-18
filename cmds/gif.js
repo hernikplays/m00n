1bfaa0728a17f8a91666f0aa97159354358a07e7
Hernikplays <hernik@hernikplays.cz>
2019-10-18 14:57:50 +0200

    this

--- cmds/gif.js
+++ cmds/gif.js
@@ -2,8 +2,7 @@ const Discord = module.require("discord.js");
 const unirest = require("unirest")
 module.exports.run = async (bot, message, args) => {
 
-    unirest.get(`api.giphy.com/v1/gifs/random`)
-        .header("api-key", process.env.GIPHY)
+    unirest.get(`api.giphy.com/v1/gifs/random?api-key=${process.env.GIPHY}`)
         .header("Accept", "application/json")
         .end(function (result) {
             console.log(result.body)
