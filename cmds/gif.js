896c7275c47d6d18fc06db31988c6652c9cf3098
Hernikplays <hernik@hernikplays.cz>
2019-10-18 15:01:44 +0200

    changed link and setimage

--- cmds/gif.js
+++ cmds/gif.js
@@ -2,13 +2,13 @@ const Discord = module.require("discord.js");
 const unirest = require("unirest")
 module.exports.run = async (bot, message, args) => {
 
-    unirest.get(`api.giphy.com/v1/gifs/random?api-key=${process.env.GIPHY}`)
+    unirest.get(`api.giphy.com/v1/gifs/random?api_key=${process.env.GIPHY}`)
         .header("Accept", "application/json")
         .end(function (result) {
             console.log(result.body)
             let em = new Discord.RichEmbed()
                 .setTitle("Here is your random GIF")
-                .setImage(result.body.data)
+                .setImage(result.body.data.images.original.url)
                 .setFooter("Supplied by GIPHY")
             message.channel.send(em)
         });
