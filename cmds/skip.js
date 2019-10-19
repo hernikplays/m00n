88bb2e3efc9bdc03513426f5c8215d732266acb8
Henry <hernik@hernikplays.cz>
2019-10-19 14:52:34 +0200

    woops

--- cmds/skip.js
+++ cmds/skip.js
@@ -1,6 +1,8 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args, ops) => {
+    if (!message.author.id == "145973959127597057") return message.reply(":x: This command is currently in beta. But you can donate to my patreon https://patreon.com/hernikplays and get access!")
+
     let fetched = ops.active.get(message.guild.id)
 
     if (!fetched) {
