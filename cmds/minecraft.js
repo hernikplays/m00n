6163668d316aaca491020cc2b843c7d3168ff783
hernikplays <hernik@hernikplays.cz>
2019-11-24 12:35:35 +0100

    Editing

--- cmds/minecraft.js
+++ cmds/minecraft.js
@@ -1,16 +1,12 @@
 let Discord = module.require("discord.js");
 const snekfetch = require("snekfetch");
 module.exports.run = async (bot, message, args) => {
-    if(!args[0]) return message.reply("Specify text please")
+    if(!args[0]) return message.channel.send(":x: You need to enter some text!")
     let [title, contents] = args.join(" ").split("|");
   if(!contents) {
     [title, contents] = ["Achievement Get!", title];
   }
   let rnd = Math.floor((Math.random() * 39) + 1);
-  if(args.join(" ").toLowerCase().includes("burn")) rnd = 38;
-  if(args.join(" ").toLowerCase().includes("cookie")) rnd = 21;
-  if(args.join(" ").toLowerCase().includes("cake")) rnd = 10;
-  if(args.join(" ").toLowerCase().includes("diamond")) rnd = 29;
 
 //  if(title.length > 10 || contents.length > 10) return message.channel.send("Max Length: 10 Characters.");
   const url = `https://www.minecraftskinstealer.com/achievement/a.php?i=${rnd}&h=${encodeURIComponent(title)}&t=${encodeURIComponent(contents)}`;
