62ce1ca2ea57aa1dc2b89cbb18faa560c78571fd
hernikplays <hernik@hernikplays.cz>
2019-12-08 12:19:04 +0100

    A-Ok!

--- cmds/anime.js
+++ cmds/anime.js
@@ -6,7 +6,6 @@ module.exports.run = async(bot, message, args) => {
         await unirest.get(`https://kitsu.io/api/edge/anime?filter[text]=${search}`)
             .header("Accept", "application/vnd.api+json")
             .end(function(result) {
-                console.log(result.body)
                 if(result.body.meta.count == 0) return message.channel.send(":x: Nothing found")
                 let e = new Discord.RichEmbed()
                     .setTitle(`Here's what I found`)
@@ -14,7 +13,7 @@ module.exports.run = async(bot, message, args) => {
                     .addField("English Title", result.body.data[0].attributes.titles.en, true)
                     .addField("Japanese Title", result.body.data[0].attributes.titles.en_jp, true)
                     .addField("Description", result.body.data[0].attributes.synopsis, true)
-                    .addField("Episode Count", result.body.data[0].attributes.episodeCount)
+                    .addField("Episode Count", result.body.data[0].attributes.episodeCount, true)
                     .setColor("RANDOM")
                     //.setFooter(`Requested by ${message.author.username}`)
                 message.channel.send(e);
