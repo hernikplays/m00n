f002ecf88ad52f917dd8eaed7da2b04b0bd9e86b
hernikplays <hernik27@gmail.com>
2019-12-19 16:23:30 +0100

    removed server count from ping

--- cmds/ping.js
+++ cmds/ping.js
@@ -1,31 +1,6 @@
 const Discord = module.require("discord.js");
-const DBL = require("dblapi.js") //DBL api
-const unirest = require("unirest")
-const BOATS = require('boats.js'); //discord.boats API
-const Boats = new BOATS(process.env.BOATS);
 
 module.exports.run = async(bot, message, args) => {
-    // SERVER COUNT POST START
-    const dbl = new DBL(process.env.DBL_TKN, bot);
-    dbl.on('posted', () => {
-        console.log('Server count posted!');
-    })
-
-    dbl.on('error', e => {
-        console.log(`Oops! ${e}`);
-    })
-    unirest.post('https://botsfordiscord.com/api/bot/481894520741691393')
-        .headers({ 'Accept': 'application/json', 'Content-Type': 'application/json' })
-        .send({ "count": bot.guilds.size, "Authorization": process.env.BFD_TKN })
-        .end(function(response) {
-            console.log("Server count posted to B4D");
-        });
-        Boats.postStats(bot.guilds.size, '481894520741691393').then(() => {
-            console.log('Successfully updated server count on discord.boats.');
-        }).catch((err) => {
-            console.error(err);
-        });
-    // SERVER COUNT POST END
     let m = await message.channel.send("Pinging...");
 
     let embed = new Discord.RichEmbed()
